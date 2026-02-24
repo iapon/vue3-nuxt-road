b70341ec78c4ce394f535e4fed9d0d2c373211ac
Evgeniy Solomanidin <evgeniysolomanidin@gmail.com>
2026-02-25 02:30:44 +0300

    day one

---
+++ day1/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Greeting from './Greeting.vue'
+</script>
+
+<template>
+  <Greeting />
+</template>
+
+<style scoped></style>
---
+++ day1/src/Greeting.vue
@@ -0,0 +1,13 @@
+<template>
+    <label v-if="!name" for="name">Введите ваше имя
+        <input id="name" v-model.lazy="name" />
+    </label>
+    <p v-else>
+        Привет, {{ name }}!
+    </p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const name = ref("");
+</script>
