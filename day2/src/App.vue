6450f94ecf45e7e523d0776438d3166bfdbe150f
Evgeniy Solomanidin <evgeniysolomanidin@gmail.com>
2026-02-25 17:46:09 +0300

    day2 passed

---
+++ day2/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Counter from './Counter.vue';
+
+</script>
+
+<template>
+  <div class="container">
+    <Counter />
+  </div>
+
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ day2/src/Counter.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+const counter = ref(0);
+
+const minus = () => {
+    if (counter.value > 0) {
+        counter.value--;
+    }
+};
+const plus = () => {
+    counter.value++;
+};
+const reset = () => {
+    counter.value = 0;
+};
+
+</script>
+
+
+<template>
+    <div class="container">
+        <div class="counter">
+            {{ counter }}
+        </div>
+        <div class="buttons">
+            <button @click="minus" :disabled="counter <= 0">-</button>
+            <button @click="reset" :disabled="counter <= 0">Сброс</button>
+            <button @click="plus">+</button>
+        </div>
+    </div>
+
+</template>
