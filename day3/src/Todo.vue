b628aa20e618a85b66f8812eb99ef7674566e261
Evgeniy Solomanidin <evgeniysolomanidin@gmail.com>
2026-02-26 16:40:33 +0300

    fix: fully implement day3

--- day3/src/Todo.vue
+++ day3/src/Todo.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const todoList = ref([]);
 const newTodoText = ref('');
@@ -13,17 +13,13 @@ const addTodo = () => {
     newTodoText.value = '';
 }
 const removeTodo = (id) => {
-    const index = todoList.value.findIndex((i) => i.id == id);
-    todoList.value.splice(index, 1);
-}
-const setTodoDone = (id) => {
-    todoList.value.find(id).done = true;
+    todoList.value.splice(todoList.value.findIndex((i) => i.id == id), 1);
 }
 const handleKeyEnter = (e) => {
-    if (e.key == 'Enter') {
-        addTodo();
-    }
+    if (e.key == 'Enter') addTodo();
 }
+const total = computed(() => { return todoList.value.length })
+const totalDone = computed(() => { return todoList.value.filter((i) => i.isDone === true).length })
 </script>
 <template>
     <h2>Список задач</h2>
@@ -33,12 +29,16 @@ const handleKeyEnter = (e) => {
     </div>
     <div class="list">
         <div v-for="t in todoList" :key="t.id" class="item">
-            <input type="checkbox" v-model="t.done" @change="setTodoDone(t.id)" />
-            <span :class="t.done ? 'done' : ''">{{ t.text }}</span>
+            <input type="checkbox" v-model="t.isDone" />
+            <span :class="t.isDone ? 'done' : ''">{{ t.text }}</span>
             <button @click=" removeTodo(t.id)">x</button>
         </div>
     </div>
-
+    <div class="result">
+        <span>Всего: {{ total }}</span>
+        <span> | </span>
+        <span>Выполнено: {{ totalDone }}</span>
+    </div>
 
 </template>
 
