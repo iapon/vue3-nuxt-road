4b03205e330a03d3876042102763ce741ffb5f82
Evgeniy Solomanidin <evgeniysolomanidin@gmail.com>
2026-02-28 02:46:35 +0300

    feat: day4

---
+++ day4/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Todo from './Todo.vue';
+</script>
+
+
+<template>
+  <div class="container">
+    <Todo />
+  </div>
+
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
---
+++ day4/src/Todo.vue
@@ -0,0 +1,162 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const todoList = ref([]);
+const newTodoText = ref('');
+
+const addTodo = () => {
+    if (newTodoText.value == '') {
+        alert("Введите текст");
+        return;
+    }
+    todoList.value.push({ id: 'item-' + new Date(), text: newTodoText.value, isDone: false });
+    newTodoText.value = '';
+}
+const removeTodo = (id) => {
+    todoList.value.splice(todoList.value.findIndex((i) => i.id == id), 1);
+}
+const handleKeyEnter = (e) => {
+    if (e.key == 'Enter') addTodo();
+}
+const total = computed(() => { return todoList.value.length });
+const totalDone = computed(() => { return todoList.value.filter((i) => i.isDone === true).length });
+
+const currentFilter = ref('all');
+const setFilter = (filter) => {
+    currentFilter.value = filter
+}
+const filteredList = computed(() => {
+    switch (currentFilter.value) {
+        case 'active':
+            return todoList.value.filter((i) => !i.isDone);
+        case 'done':
+            return todoList.value.filter((i) => i.isDone);
+        default:
+            return todoList.value
+    }
+
+
+})
+
+
+
+</script>
+<template>
+    <h2>Список задач</h2>
+    <div class="filters">
+        <button @click="setFilter('all')" :class="currentFilter == 'all' ? 'active' : ''">Все</button>
+        <button @click="setFilter('active')" :class="currentFilter == 'active' ? 'active' : ''">Активные</button>
+        <button @click="setFilter('done')" :class="currentFilter == 'done' ? 'active' : ''">Выполненные</button>
+    </div>
+    <div class="action">
+        <input @keypress="handleKeyEnter" type="text" name="todotext" v-model="newTodoText">
+        <button @click="addTodo">Добавить</button>
+    </div>
+    <div class="list">
+        <div v-for="t in filteredList" :key="t.id" class="item">
+            <input type="checkbox" v-model="t.isDone" />
+            <span :class="t.isDone ? 'done' : ''">{{ t.text }}</span>
+            <button @click=" removeTodo(t.id)">x</button>
+        </div>
+    </div>
+    <div class="result">
+        <span>Всего: {{ total }}</span>
+        <span> | </span>
+        <span>Выполнено: {{ totalDone }}</span>
+    </div>
+
+</template>
+
+<style scoped>
+.filters {
+    display: flex;
+    gap: 1rem;
+    max-width: 15rem;
+
+    /* From Uiverse.io by aasisodiya */
+    button {
+        background-color: #eee;
+        border: none;
+        padding: 1rem;
+        font-size: 1rem;
+        width: 10em;
+        border-radius: 1rem;
+        color: lightcoral;
+        box-shadow: 0 0.4rem #dfd9d9;
+        cursor: pointer;
+    }
+
+    button:active {
+        color: white;
+        box-shadow: 0 0.2rem #dfd9d9;
+        transform: translateY(0.2rem);
+    }
+
+    button:hover:not(:disabled) {
+        background: lightcoral;
+        color: white;
+        text-shadow: 0 0.1rem #bcb4b4;
+    }
+
+    button:disabled {
+        cursor: auto;
+        color: grey;
+    }
+}
+
+.action {
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+    max-width: 15rem;
+
+    & input {
+        flex: 1;
+    }
+}
+
+.list {
+    display: flex;
+    flex-direction: column;
+    max-height: 50rem;
+    overflow-y: auto;
+    min-width: 15rem;
+    gap: 0.5rem;
+    padding: 0.5rem;
+
+    & .item {
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        justify-content: center;
+        align-items: center;
+
+        & input {
+            width: 1.25rem;
+            height: 1.25rem;
+        }
+
+        & span {
+            width: 100%;
+
+            &.done {
+                text-decoration: line-through;
+            }
+        }
+
+        & button {
+            text-align: center;
+            vertical-align: center;
+            transition: all 0.5s ease;
+            border: 0;
+            border-radius: 20%;
+            width: 1.25rem;
+            height: 1.25rem;
+
+            &:hover {
+                background-color: gray;
+            }
+        }
+    }
+}
+</style>
