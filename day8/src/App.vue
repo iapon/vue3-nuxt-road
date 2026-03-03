7ca89bd5656a06ea89e5235c4e2fe551334a0ba2
Evgeniy Solomanidin <evgeniysolomanidin@gmail.com>
2026-03-03 17:18:05 +0300

    feat: day8

---
+++ day8/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import UserCard from './UserCard.vue';
+
+const users = [{
+  id: 1,
+  name: "John Doe",
+},
+{
+  id: 2,
+  name: "John Doe",
+  email: "john@email.com",
+  role: "user",
+  avatar: "https://i.pravatar.cc/300"
+},
+{
+  id: 3,
+  name: "Super Admin",
+  email: "admin@email.com",
+  role: "admin",
+  avatar: "https://i.pravatar.cc/300",
+}
+]
+</script>
+
+<template>
+  <div class="flex flex-col items-center bg-gray-200 min-h-dvh min-w-dvw">
+    <UserCard v-for="user in users" :key="user.id" :name="user.name" :email="user.email" :role="user.role"
+      :avatar="user.avatar" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ day8/src/UserCard.vue
@@ -0,0 +1,45 @@
+<!-- - Props: name (обязательный), email (по умолчанию 'email@example.com'), role (по умолчанию 'user')
+- Если avatar не передан — показать круг с первой буквой имени
+- Роль отображается в цветном бейдже
+- Карточка имеет рамку и отступы -->
+
+<script setup>
+
+defineProps({
+    name: {
+        type: String,
+        required: true,
+
+    },
+    email: {
+        type: String,
+        default: 'email@example.com',
+    },
+    role: {
+        type: String,
+        default: 'user',
+    },
+    avatar: {
+        type: String
+    }
+})
+
+</script>
+
+<template>
+    <div class="flex flex-col w-full h-full max-h-sm max-w-md rounded-2xl shadow-2xl p-4 mt-8 bg-white">
+        <div class="flex gap-1 relative">
+            <img class="w-16 h-16 rounded-full" v-if="avatar" :src="avatar" alt="avatar">
+            <div v-else class="rounded-full bg-gray-600 w-16 h-16 p-2 flex justify-center items-center">
+                <span class="text-2xl text-white">{{ name[0].toUpperCase() }}</span>
+            </div>
+            <div class="flex ml-4 flex-col justify-between items-start">
+                <span class="text-md ml-4 truncate">{{ name }}</span>
+                <span class="text-xl  ml-4 truncate">{{ email }}</span>
+
+            </div>
+            <span class="absolute -top-1 rounded-xl -right-1 px-3 py-1 bg-slate-600 text-white">{{ role }}</span>
+
+        </div>
+    </div>
+</template>
