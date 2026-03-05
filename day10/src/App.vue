701f0cb9c5b2ab0e0ffbceac337f7be817981461
Evgeniy Solomanidin <evgeniysolomanidin@gmail.com>
2026-03-06 02:39:03 +0300

    feat: day10

---
+++ day10/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Card from './Card.vue';
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center min-h-dvh gap-12 mt-10">
+    <Card title="Первая карточка">
+      <template #default>
+        <p class="text-xl font-bold">
+          Это не текст по умолчанию
+        </p>
+      </template>
+    </Card>
+    <Card>
+      <template #header>
+        <h1 class="font-bold">ЭТО ТАЙТЛ!</h1>
+      </template>
+    </Card>
+    <Card>
+      <template #header>
+        <h1 class="font-bold">ЭТО НОВЫЙ ТАЙТЛ!</h1>
+      </template>
+      <template #default>
+        <p>
+          Это всё еще не текст по умолчанию
+        </p>
+      </template>
+      <template #footer>
+        <p class="text-sm">
+          Это футер
+        </p>
+      </template>
+    </Card>
+  </div>
+</template>
---
+++ day10/src/Card.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface CardProps {
+    title?: String
+}
+defineProps<CardProps>()
+</script>
+
+
+
+<template>
+    <div class="w-md shadow-md rounded-2xl flex flex-col gap-2 p-8 border-gray-100 border">
+        <div class="w-full">
+            <slot name="header">{{ title }}</slot>
+        </div>
+        <div class="flex-1">
+            <slot>
+                <p>
+                    Это текст по умолчанию
+                </p>
+            </slot>
+        </div>
+        <div class="mt-auto" v-if="$slots.footer">
+            <slot name="footer"></slot>
+        </div>
+    </div>
+</template>
