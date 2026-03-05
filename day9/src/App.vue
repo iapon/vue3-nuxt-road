36bd70d524e900406b1c623e9dcf85ce3a97b453
Evgeniy Solomanidin <evgeniysolomanidin@gmail.com>
2026-03-05 03:00:43 +0300

    feat: day9

---
+++ day9/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Rating from './Rating.vue';
+const rating = ref(0);
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-10">
+    <Rating v-model="rating" />
+    <p>Ваша оценка: {{ rating }} из 5</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ day9/src/Rating.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+
+<template>
+    <div class="flex flex-row-reverse  justify-end text-2xl text-gray-300 ">
+        <span v-for="i in [5, 4, 3, 2, 1]" class="w-10 h-10 text-center relative inline-block text-5xl cursor-pointer 
+              hover:text-yellow-400 
+              [&:hover~span]:text-yellow-400" :class="props.modelValue > i - 1 ? 'text-yellow-400 ' : ''"
+            @click="emit('update:modelValue', i == props.modelValue ? 0 : i)">☆</span>
+    </div>
+</template>
